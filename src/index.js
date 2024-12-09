52715eb7ef4c3ce0e8b66eb669dec5203fb9a337
Lawrencia Baaba Mensah <lawbabmensah@gmail.com>
2024-12-09 12:35:10 +0000

    added new changes

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// let image = document.getElementById("img");
+// img.inner.HTML = `<img src =
+function changeButtonstyle(){
+button.style.backgroundColor = ("yellow")
+button.style.color = ("white")
+}
+let button = document.querySelector("button");
+button.addEventListener("click",changeButtonstyle);
+
+let but_img = 
